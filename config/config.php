d6dafb5f56d7a4cf29e7f72ea50fade79706d5b7
deemonic <michael.deeming90@gmail.com>
2025-03-04 09:58:25 +0000

    updated config file

--- config/config.php
+++ config/config.php
@@ -20,31 +20,125 @@ return [
     | Onym Generation Options
     |--------------------------------------------------------------------------
     |
-    | Configure how converted files should be named by default.
-    | Available strategies: 'original', 'random', 'uuid', 'timestamp', 'date',
-    | 'prefix', 'suffix', 'numbered', 'slug', 'hash'
+    | Configure the options for each strategy.
+    | These options are used to generate the new filename.
+    |
+    | Available options:
+    | - length: The length of the random string.
+    | - format: The format of the timestamp or date.
+    | - prefix: The prefix of the filename.
+    | - suffix: The suffix of the filename.
+    | - number: The number of the filename.
+    | - separator: The separator of the numbered filename.
+    | - algorithm: The algorithm of the hash.
     |
     */
     'options' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Random Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the random strategy.
+        |
+        | Available options:
+        | - length: The length of the random string.
+        |
+        */
         'random' => [
             'length' => 16,
         ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Timestamp Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the timestamp strategy.
+        |
+        | Available options:
+        | - format: The format of the timestamp.
+        |
+        */
         'timestamp' => [
             'format' => 'Y-m-d_H-i-s',
         ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Date Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the date strategy.
+        |
+        | Available options:
+        | - format: The format of the date.
+        |
+        */
         'date' => [
             'format' => 'Y-m-d',
         ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Prefix Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the prefix strategy.
+        |
+        | Available options:
+        | - prefix: The prefix of the filename.
+        |
+        */
         'prefix' => [
             'prefix' => 'converted_',
         ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Suffix Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the suffix strategy.
+        |
+        | Available options:
+        | - suffix: The suffix of the filename.
+        |
+        */
         'suffix' => [
             'suffix' => '_converted',
         ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Numbered Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the numbered strategy.
+        |
+        | Available options:
+        | - number: The number of the filename.
+        | - separator: The separator of the numbered filename.
+        |
+        */
         'numbered' => [
             'number' => 1,
             'separator' => '_',
         ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Hash Strategy Options
+        |--------------------------------------------------------------------------
+        |
+        | Configure the options for the hash strategy.
+        |
+        | Available options:
+        | - algorithm: The algorithm of the hash.
+        | - length: The length of the hash.
+        |
+        */
         'hash' => [
             'algorithm' => 'md5',
             'length' => 16,
--- src/Onym.php
+++ src/Onym.php
@@ -8,16 +8,33 @@ use Illuminate\Support\Str;
 
 class Onym
 {
+    /**
+     * The strategy to use.
+     *
+     * @var string
+     */
     public string $strategy;
 
-    public array $options;
-
+    /**
+     * The options to use.
+     *
+     * @var array
+     */
     public function __construct()
     {
         $this->strategy = config('onym.strategy');
         $this->options = config('onym.options');
     }
 
+    /**
+     * Generate a new filename based on the strategy and options.
+     *
+     * @param string|null $originalFilename The original filename.
+     * @param string|null $extension The extension of the file.
+     * @param string|null $strategy The strategy to use.
+     * @param array|null $options The options to use.
+     * @return string The new filename.
+     */
     public function make(
         ?string $originalFilename, 
         ?string $extension, 
@@ -50,16 +67,37 @@ class Onym
         };
     }
 
+    /**
+     * Generate a random string of characters.
+     *
+     * @param int|null $length The length of the random string.
+     * @param string $extension The extension of the file.
+     * @return string The random string.
+     */
     public function random(?int $length = 16, $extension)
     {
         return Str::random($options['length'] ?? 16) . '.' . $extension;
     }
 
+    /**
+     * Generate a UUID string.
+     *
+     * @param string $extension The extension of the file.
+     * @return string The UUID string.
+     */
     public function uuid($extension)
     {
         return (string) Str::uuid() . '.' . $extension;
     }
 
+    /**
+     * Generate a timestamp string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @param array $options The options array.
+     * @return string The timestamp string.
+     */
     public function timestamp($originalFilename, $extension, $options)
     {
         $format = $options['format'] ?? 'Y-m-d_H-i-s';
@@ -67,6 +105,14 @@ class Onym
         return $date->format($format) . '_' . $originalFilename . '.' . $extension;
     }
 
+    /**
+     * Generate a date string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @param array $options The options array. 
+     * @return string The date string.
+     */
     public function date($originalFilename, $extension, $options)
     {
         $format = $options['format'] ?? 'Y-m-d';
@@ -74,27 +120,66 @@ class Onym
         return $date->format($format) . '_' . $originalFilename . '.' . $extension;
     }
 
+    /**
+     * Generate a prefix string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @param array $options The options array.
+     * @return string The prefix string.    
+     */
     public function prefix($originalFilename, $extension, $options)
     {
         return ($options['prefix'] ?? 'converted_') . $originalFilename . '.' . $extension;
     }
 
+    /**
+     * Generate a suffix string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @param array $options The options array.
+     * @return string The suffix string.
+     */
     public function suffix($originalFilename, $extension, $options)
     {
         return $originalFilename . ($options['suffix'] ?? '_converted') . '.' . $extension;
     }
 
+    /**
+     * Generate a numbered string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @param array $options The options array.
+     * @return string The numbered string.
+     */
     public function numbered($originalFilename, $extension, $options)
     {
         $number = $options['number'] ?? 1;
         return $originalFilename . '_' . $number . '.' . $extension;
     }
 
+    /**
+     * Generate a slug string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @return string The slug string.
+     */
     public function slug($originalFilename, $extension)
     {
         return Str::slug($originalFilename) . '.' . $extension;
     }
 
+    /**
+     * Generate a hash string.
+     *
+     * @param string $originalFilename The original filename.
+     * @param string $extension The extension of the file.
+     * @param array $options The options array.
+     * @return string The hash string.
+     */
     public function hash($originalFilename, $extension, $options)
     {
         $hash = hash($options['algorithm'] ?? 'sha256', $originalFilename);
